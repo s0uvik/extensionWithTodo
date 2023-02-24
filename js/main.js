fbfd8809e3c2a749cef7cca83111c44f14a2f544
s0uvik <souviksarkar260123@gmail.com>
2023-02-24 15:36:09 +0530

    message2

--- js/main.js
+++ js/main.js
@@ -10,21 +10,6 @@ fetch(
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
   });
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
-  .then((res) => {
-    if (!res.ok) {
-      throw Error("Something went wrong");
-    }
-    console.log(res.status);
-    return res.json();
-  })
-  .then((data) => {
-    document.querySelector(
-      ".crypto"
-    ).textContent = `Dogecoin: ₹${data.market_data.current_price.inr}`;
-  })
-  .catch((err) => console.error(err));
-
 function getCurrentTime() {
   const date = new Date();
   document.querySelector(".time").textContent = date.toLocaleTimeString({
